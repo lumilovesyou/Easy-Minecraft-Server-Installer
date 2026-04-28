910cb07585b0992f106c6fdd57e6f7ab0dad5e15
mewo <felis.araneae@gmail.com>
2026-04-28 14:33:11 -0500

    Restructuring to New Goals

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use::inquire::{
 };
 use reqwest::blocking::get;
 use serde::Deserialize;
+use serde_json::Value;
 use url::{
     Url
 };
@@ -33,9 +34,9 @@ fn selectChoice(prompt: &str, options: Vec<String>) -> String {
         };
 }
 
-fn enterChoice(prompt: &str, condition: fn(&str) -> bool) -> String {
+fn enterChoice<F>(prompt: &str, condition: F) -> String where F: Fn(&str) -> &str, {
     return match Text::new(prompt)
-        .with_validator(|input: &str| { if condition(input) { Ok(Validation::Invalid("Field cannot be empty!".into())) } else { Ok(Validation::Valid) }})
+        .with_validator(|input: &str| { if !condition(input).is_empty() { Ok(Validation::Invalid(condition(input).into())) } else { Ok(Validation::Valid) }})
         .prompt() {
             Ok(val) => val,
             Err(_e) => {
@@ -44,6 +45,46 @@ fn enterChoice(prompt: &str, condition: fn(&str) -> bool) -> String {
         };
 }
 
+fn stringList(list: Vec<&str>) -> Vec<String> {
+    list.into_iter().map(|s| s.to_string()).collect()
+}
+
+fn isEmpty(text: &str) -> &str {
+    if text.is_empty() {
+        return "Cannot be empty";
+    }
+    return "";
+}
+
+fn modpackIsEmpty(text: &str, modpackURL: String) -> &str {
+    if text.is_empty() && modpackURL.is_empty() {
+        return "Cannot be empty";
+    }
+    return "";
+}
+
+fn isValidModpackLink(url: &str) -> &str {
+    if url.is_empty() { return "" };
+    if Url::parse(url).is_ok() {
+        let parsedUrl = Url::parse(url).unwrap();
+        match parsedUrl.host_str() {
+            Some("modrinth.com") => {
+                let segments = parsedUrl.path_segments().map(|s| s.collect::<Vec<_>>()).unwrap();
+                println!("{:?}", segments);
+                //https://modrinth.com/modpack/fabulously-optimized
+                //https://api.modrinth.com/v2/project/1KVo5zza/version
+                if segments[0] == "modpack" && segments.len() > 1 { return "" }
+            },
+            Some("curseforge.com") => {
+
+            },
+            _ => {}
+        }
+    }
+    return "Invalid modpack URL";
+}
+
+//API Stuff
 fn getVersions(launcher: &String, filter: bool) -> Vec<String> {
     match launcher.as_str() {
         "Fabric" => {
@@ -63,44 +104,26 @@ fn getVersions(launcher: &String, filter: bool) -> Vec<String> {
     }
 }
 
-fn stringList(list: Vec<&str>) -> Vec<String> {
-    list.into_iter().map(|s| s.to_string()).collect()
-}
-
-fn isEmpty(text: &str) -> bool {
-    text.is_empty()
-}
-
-fn isntValidUrl(url: &str) -> bool {
-    !Url::parse(url).is_ok() || url.is_empty()
-}
-
-fn error(text: String) {
-    println!("{}", text);
-    exit(0);
-}
-
-fn isValidModpackLink(url: &str) -> bool {
-    if Url::parse(url).is_ok() {
-        let parsedUrl = Url::parse(url).unwrap();
-        match parsedUrl.host_str() {
-            Some("modrinth.com") => {
-                let mut segments = parsedUrl.path_segments().map(|s| s.collect::<Vec<_>>()).unwrap();
-                //https://modrinth.com/modpack/fabulously-optimized
-                //https://api.modrinth.com/v2/project/1KVo5zza/version
-                if segments[0] != "modpack" { error(format!("Invalid Modrinth URL \"{}\"!", url)); }
-                segments.truncate(2);
-            },
-            Some("curseforge.com") => {
-
-            },
-            _ => {}
-        }
-    }
-    return false
+fn getURL(url: String) -> Value {
+    let parsedURL = Url::parse(&url).unwrap();
+    return get(url).unwrap().json().unwrap();
 }
 
 fn main() {
+    let modpackURL = enterChoice("Enter a modpack URL (optional)", isValidModpackLink);
+    let mut modpackJSON: Value = Value::Null;
+    if !modpackURL.is_empty() {
+        let parsed = Url::parse(&modpackURL).unwrap();
+        let modpackTitle = parsed.path_segments().unwrap().nth(1).unwrap();
+        modpackJSON = getURL(format!("https://api.modrinth.com/v2/project/{}", modpackTitle));
+    }
+    let mut serverName = enterChoice("Enter the server's name", |input| modpackIsEmpty(input, modpackURL.clone()));
+    if serverName.is_empty() {
+        serverName = modpackJSON["title"].as_str().unwrap().to_string();
+    }
+    println!("Modpack URL: {}\nServer Name: {}", modpackURL, serverName);
+
+    /*
     let name = enterChoice("Enter server name:", isEmpty);
 
     let selectedLauncher = selectChoice("Select a launcher:", stringList(vec!["Fabric", "Neoforge", "Quilt", "Forge"]));
@@ -118,6 +141,7 @@ fn main() {
     }
 
     println!("Output:\nName: {}\nLauncher: {}\nVersion: {}\nModpack: {}", name, selectedLauncher, selectedVersion, modpackLink);
+    */
 }
 
 ////Process:
